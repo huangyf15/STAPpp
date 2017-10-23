1b348b32204d1b4e680bd84c9d91609112e24e9a
gwy15 <gwy15thu@gmail.com>
2017-10-23 23:20:02 +0800

    用 < 代替 <= x - 1

--- src/cpp/Solver.cpp
+++ src/cpp/Solver.cpp
@@ -30,19 +30,19 @@ void CLDLTSolver::LDLT()
         // Row number of the first non-zero element in column j (Numbering starting from 1)
 		unsigned int mj = j - ColumnHeights[j-1];
         
-		for (unsigned int i = mj+1; i <= j-1; i++)	// Loop for mj+1:j-1 (Numbering starting from 1)
+		for (unsigned int i = mj+1; i < j; i++)	// Loop for mj+1:j-1 (Numbering starting from 1)
 		{
             // Row number of the first nonzero element in column i (Numbering starting from 1)
 			unsigned int mi = i - ColumnHeights[i-1];
 
 			double C = 0.0;
-			for (unsigned int r = max(mi, mj); r <= i-1; r++)
+			for (unsigned int r = max(mi, mj); r < i; r++)
 				C += K(r,i) * K(r,j);		// C += L_ri * U_rj
 
 			K(i,j) -= C;	// U_ij = K_ij - C
 		}
 
-		for (unsigned int r = mj; r <= j-1; r++)	// Loop for mj:j-1 (column j)
+		for (unsigned int r = mj; r < j; r++)	// Loop for mj:j-1 (column j)
 		{
 			double Lrj = K(r,j) / K(r,r);	// L_rj = U_rj / D_rr
 			K(j,j) -= Lrj * K(r,j);	// D_jj = K_jj - sum(L_rj*U_rj, r=mj:j-1)
@@ -71,7 +71,7 @@ void CLDLTSolver::BackSubstitution(double* Force)
 	{
         unsigned int mi = i - ColumnHeights[i-1];
 
-		for (unsigned int j = mi; j <= i-1; j++)	// Loop for j=mi:i-1
+		for (unsigned int j = mi; j < i; j++)	// Loop for j=mi:i-1
 			Force[i-1] -= K(j,i) * Force[j-1];	// V_i = R_i - sum_j (L_ji V_j)
 	}
 
@@ -83,7 +83,7 @@ void CLDLTSolver::BackSubstitution(double* Force)
 	{
         unsigned int mj = j - ColumnHeights[j-1];
 
-		for (unsigned int i = mj; i <= j-1; i++)	// Loop for i=mj:j-1
+		for (unsigned int i = mj; i < j; i++)	// Loop for i=mj:j-1
 			Force[i-1] -= K(i,j) * Force[j-1];	// a_i = Vbar_i - sum_j(L_ij Vbar_j)
 	}
 };
