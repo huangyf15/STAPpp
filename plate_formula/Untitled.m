970ac318d504bd5121ee36a59d0473f912b47289
yangzy1998 <yangzy_1998@163.com>
2017-12-01 13:40:14 +0800

    other modifications for rectangles

--- plate_formula/Untitled.m
+++ plate_formula/Untitled.m
@@ -1,6 +1,6 @@
-syms a b s t Dst nu Dxr Dyr D1r Dxyr p;
+syms a b s t Dst nu Dxr Dyr D1r Dxyr p xpsi yeta;
 NT=[-(s-1)*(t-1)*(s^2+s+t^2+t-2)/8;
-    -b*(s-1)*(t-1)^2*(t+1);
+    -b*(s-1)*(t-1)^2*(t+1)/8;
     a*(s-1)^2*(s+1)*(t-1)/8;
     (s+1)*(t-1)*(s^2-s+t^2+t-2)/8;
     b*(s+1)*(t-1)^2*(t+1)/8;
@@ -17,9 +17,11 @@ Bnew=[-diff(NT,s,2)/a^2 -diff(NT,t,2)/b^2 -2*diff(diff(NT,s,1),t,1)/a/b];
 Cmat=[1 nu 0;nu 1 0; 0 0 (1-nu)/2];
 Koriginal=Bnew*Cmat*conj(Bnew');
 %Kall=a*b*int(int(Bnew*Cmat*conj(Bnew'),t,-1,1),s,-1,1);
-Kall=a*b*int(int(Koriginal,t,-1,1),s,-1,1);
+%Kall=a*b*int(int(Koriginal,t,-1,1),s,-1,1);
+Kall=k*int(int(Koriginal,t,-1,1),s,-1,1);
 
-fileused=fopen('record.txt','wt');
+Kall=subs(Kall,{a,b},{xpsi,yeta});
+fileused=fopen('rect001.txt','wt');
 for j=1:12
     for i=j:-1:1
         fprintf(fileused,'%s%i%s%s%s\n','Matrix[',j*(j+1)/2-i,'] = ',char(Kall(i,j)),';');
