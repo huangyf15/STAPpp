207a2b2591356979f53c01176344c78bb5741e54
gwy15 <gwy15thu@gmail.com>
2017-12-09 11:58:03 +0800

    完善手动测试脚本

--- test/test.bat
+++ test/test.bat
@@ -14,20 +14,26 @@
 @make rebuild_cache
 @make
 @stap++ ../data/test_truss_22.dat > nul
-@if not %errorlevel%==0 (echo run test failed. & exit)
+@if not %errorlevel%==0 goto fail
 @stap++ ../data/truss > nul
-@if not %errorlevel%==0 (echo run test failed. & exit)
+@if not %errorlevel%==0 goto fail
 @stap++ ../data/bar-6 > nul
-@if not %errorlevel%==0 (echo run test failed. & exit)
+@if not %errorlevel%==0 goto fail
 @cd ../data/4Q
 @python run-patch.py
-@if not %errorlevel%==0 (echo test failed. & exit)
+@if not %errorlevel%==0 goto fail
 @cd ../3T
 @python run-patch.py
-@if not %errorlevel%==0 (echo test failed. & exit)
+@if not %errorlevel%==0 goto fail
 @cd ../Beam
 @python run-patch.py
-@if not %errorlevel%==0 (echo test failed. & exit)
+@if not %errorlevel%==0 goto fail
 @cd ../8H
 @python run-patch.py
-@if not %errorlevel%==0 (echo test failed. & exit)
+@if not %errorlevel%==0 goto fail
+@echo test success
+@goto quit
+
+:fail
+@echo test failed
+:quit
