26dd96c8bdcf3fa075e6e6e96d2372ed2f00e4a3
gwy15 <gwy15thu@gmail.com>
2017-12-11 00:16:26 +0800

    resolve domain.h

--- src/h/Domain.h
+++ src/h/Domain.h
@@ -10,166 +10,168 @@
 
 #pragma once
 
-#include <cstddef>
-#include <fstream>
 #include <string>
+#include <fstream>
 #include <vector>
+#include <cstddef>
 
+#include "Node.h"
 #include "ElementGroup.h"
 #include "Outputter.h"
 #include "Solver.h"
 #include "LoadCaseData.h"
-#include "Node.h"
-#include "Outputter.h"
 #include "SkylineMatrix.h"
 
 using namespace std;
 
 //!	Clear an array
-template <class type> void clear(type* a, unsigned int N);
+template <class type> void clear( type* a, unsigned int N );
 
 //!	Domain class : Define the problem domain
 /*!	Only a single instance of Domain class can be created */
 class CDomain
 {
 private:
-    //!	The instance of the Domain class
-    static CDomain* _instance;
 
-    //!	Input file stream for reading data from input data file
-    ifstream Input;
+//!	The instance of the Domain class
+	static CDomain* _instance;
 
-    //!	Heading information for use in labeling the outpu
-    char Title[256];
+//!	Input file stream for reading data from input data file
+	ifstream Input;
 
-    //!	Solution MODEX
-    /*!		0 : Data check only;
-            1 : Execution */
-    unsigned int MODEX;
+//!	Heading information for use in labeling the outpu
+	char Title[256]; 
 
-    //!	Total number of nodal points
-    unsigned int NUMNP;
+//!	Solution MODEX
+/*!		0 : Data check only;
+		1 : Execution */
+	unsigned int MODEX;
 
-    //!	List of all nodes in the domain
-    CNode* NodeList;
+//!	Total number of nodal points
+	unsigned int NUMNP;
 
-    //!	Total number of element groups.
-    /*! An element group consists of a convenient collection of elements with same type */
-    unsigned int NUMEG;
+//!	List of all nodes in the domain
+	CNode* NodeList;
 
-    //! Element group list
+//!	Total number of element groups.
+/*! An element group consists of a convenient collection of elements with same type */
+	unsigned int NUMEG;
+
+//! Element group list
     CElementGroup* EleGrpList;
+    
+//!	Number of load cases
+	unsigned int NLCASE;
 
-    //!	Number of load cases
-    unsigned int NLCASE;
+//!	List of all load cases
+	CLoadCaseData* LoadCases;
 
-    //!	List of all load cases
-    CLoadCaseData* LoadCases;
+//!	Number of concentrated loads applied in each load case
+	unsigned int* NLOAD;
 
-    //!	Number of concentrated loads applied in each load case
-    unsigned int* NLOAD;
+//!	Total number of equations in the system
+	unsigned int NEQ;
 
-    //!	Total number of equations in the system
-    unsigned int NEQ;
+//!	Number of elements in banded global stiffness matrix
+	unsigned int NWK;
 
-    //!	Number of elements in banded global stiffness matrix
-    unsigned int NWK;
+//!	Maximum half bandwith
+	unsigned int MK;
 
-    //!	Maximum half bandwith
-    unsigned int MK;
-
-    //!	Banded stiffness matrix
-    /*! A one-dimensional array storing only the elements below the	skyline of the
-        global stiffness matrix. */
+//!	Banded stiffness matrix
+/*! A one-dimensional array storing only the elements below the	skyline of the 
+    global stiffness matrix. */
     CSkylineMatrix<double>* StiffnessMatrix;
 
-    //!	Global nodal force/displacement vector
-    double* Force;
+//!	Global nodal force/displacement vector
+	double* Force;
 
 public:
-    //!	Constructor
-    CDomain();
 
-    //!	Desconstructor
-    ~CDomain();
+//!	Constructor
+	CDomain();
 
-    //!	Return pointer to the instance of the Domain class
-    static CDomain* Instance();
+//!	Desconstructor
+	~CDomain();
 
-    //!	Read domain data from the input data file
-    bool ReadData(string FileName, string OutFile);
+//!	Return pointer to the instance of the Domain class
+	static CDomain* Instance();
 
-    //!	Read nodal point data
-    bool ReadNodalPoints();
+//!	Read domain data from the input data file
+	bool ReadData(string FileName, string OutFile);
 
-    //!	Read load case data
-    bool ReadLoadCases();
+//!	Read nodal point data
+	bool ReadNodalPoints();
 
-    //!	Read element data
-    bool ReadElements();
+//!	Read load case data
+	bool ReadLoadCases();
 
-    //!	Calculate global equation numbers corresponding to every degree of freedom of each node
-    void CalculateEquationNumber();
+//!	Read element data
+	bool ReadElements();
 
-    //!	Calculate column heights
-    void CalculateColumnHeights();
+//!	Calculate global equation numbers corresponding to every degree of freedom of each node
+	void CalculateEquationNumber();
 
-    //!	Calculate address of diagonal elements in banded matrix
-    void CalculateDiagnoalAddress();
+//!	Calculate column heights
+	void CalculateColumnHeights();
 
-    //! Allocate storage for matrices
-    /*!	Allocate Force, ColumnHeights, DiagonalAddress and StiffnessMatrix and
-        calculate the column heights and address of diagonal elements */
-    void AllocateMatrices();
+//!	Calculate address of diagonal elements in banded matrix
+	void CalculateDiagnoalAddress();
 
-    //!	Assemble the banded gloabl stiffness matrix
-    void AssembleStiffnessMatrix();
+//! Allocate storage for matrices
+/*!	Allocate Force, ColumnHeights, DiagonalAddress and StiffnessMatrix and 
+    calculate the column heights and address of diagonal elements */
+	void AllocateMatrices();
 
-    //!	Assemble the global nodal force vector for load case LoadCase
-    bool AssembleForce(unsigned int LoadCase);
+//!	Assemble the banded gloabl stiffness matrix
+	void AssembleStiffnessMatrix();
 
-    //!	Return solution mode
-    inline unsigned int GetMODEX() { return MODEX; }
+//!	Assemble the global nodal force vector for load case LoadCase
+	bool AssembleForce(unsigned int LoadCase); 
 
-    //!	Return the title of problem
-    inline string GetTitle() { return Title; }
+//!	Return solution mode
+	inline unsigned int GetMODEX() { return MODEX; }
 
-    //!	Return the total number of equations
-    inline unsigned int GetNEQ() { return NEQ; }
+//!	Return the title of problem
+	inline string GetTitle() { return Title; }
 
-    //!	Return the total number of nodal points
-    inline unsigned int GetNUMNP() { return NUMNP; }
+//!	Return the total number of equations
+	inline unsigned int GetNEQ() { return NEQ; }
 
-    //!	Return the number of banded global stiffness matrix elements
-    inline unsigned int GetNWK() { return NWK; }
+//!	Return the total number of nodal points
+	inline unsigned int GetNUMNP() { return NUMNP; }
 
-    //!	Return the maximum half bandwith
-    inline unsigned int GetMK() { return MK; }
+//!	Return the number of banded global stiffness matrix elements
+	inline unsigned int GetNWK() { return NWK; }
 
-    //!	Return the node list
-    inline CNode* GetNodeList() { return NodeList; }
+//!	Return the maximum half bandwith
+	inline unsigned int GetMK() { return MK; }
 
-    //!	Return total number of element groups
-    inline unsigned int GetNUMEG() { return NUMEG; }
+//!	Return the node list
+	inline CNode* GetNodeList() { return NodeList; }
 
-    //! Return element group list
+//!	Return total number of element groups
+	inline unsigned int GetNUMEG() { return NUMEG; }
+
+//! Return element group list
     CElementGroup* GetEleGrpList() { return EleGrpList; }
 
-    //!	Return pointer to the global nodal force vector
-    inline double* GetForce() { return Force; }
+//!	Return pointer to the global nodal force vector
+	inline double* GetForce() { return Force; }
 
-    //!	Return pointer to the global nodal displacement vector
-    inline double* GetDisplacement() { return Force; }
+//!	Return pointer to the global nodal displacement vector
+	inline double* GetDisplacement() { return Force; }
 
-    //!	Return the total number of load cases
-    inline unsigned int GetNLCASE() { return NLCASE; }
+//!	Return the total number of load cases
+	inline unsigned int GetNLCASE() { return NLCASE; }
 
-    //!	Return the number of concentrated loads applied in each load case
-    inline unsigned int* GetNLOAD() { return NLOAD; }
+//!	Return the number of concentrated loads applied in each load case
+	inline unsigned int* GetNLOAD() { return NLOAD; }
 
-    //!	Return the list of load cases
-    inline CLoadCaseData* GetLoadCases() { return LoadCases; }
+//!	Return the list of load cases
+	inline CLoadCaseData* GetLoadCases() { return LoadCases; }
 
-    //!	Return pointer to the banded stiffness matrix
-    inline CSkylineMatrix<double>* GetStiffnessMatrix() { return StiffnessMatrix; }
+//!	Return pointer to the banded stiffness matrix
+	inline CSkylineMatrix<double>* GetStiffnessMatrix() { return StiffnessMatrix; }
+
 };
