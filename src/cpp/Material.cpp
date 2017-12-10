e0adca647cb6d96667c2663f5386c08cbb1e1ba0
gwy15 <gwy15thu@gmail.com>
2017-12-11 00:09:31 +0800

    resolve Material.cpp

--- src/cpp/Material.cpp
+++ src/cpp/Material.cpp
@@ -10,35 +10,35 @@
 
 #include "Material.h"
 
+#include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <iostream>
 
 using namespace std;
 
 //	Read material data from stream Input
 bool CBarMaterial::Read(ifstream& Input, unsigned int mset)
 {
-    Input >> nset; // Number of property set
+	Input >> nset;	// Number of property set
 
-    if (nset != mset + 1)
-    {
-        cerr << "*** Error *** Material sets must be inputted in order !" << endl
-             << "    Expected set : " << mset + 1 << endl
-             << "    Provided set : " << nset << endl;
+	if (nset != mset + 1)
+	{
+		cerr << "*** Error *** Material sets must be inputted in order !" << endl 
+			 << "    Expected set : " << mset + 1 << endl
+			 << "    Provided set : " << nset << endl;
 
-        return false;
-    }
+		return false;
+	}
 
-    Input >> E >> Area; // Young's modulus and section area
+	Input >> E >> Area;	// Young's modulus and section area
 
-    return true;
+	return true;
 }
 
 //	Write material data to Stream
 void CBarMaterial::Write(COutputter& output, unsigned int mset)
 {
-    output << setw(5) << mset + 1 << setw(16) << E << setw(16) << Area << endl;
+	output << setw(5) << mset+1 << setw(16) << E << setw(16) << Area << endl;
 }
 
 //	Read material data from stream Input
@@ -63,20 +63,20 @@ bool CTriangleMaterial::Read(ifstream& Input, unsigned int mset)
 
 bool CQuadrilateralMaterial::Read(ifstream& Input, unsigned int mset)
 {
-    Input >> nset; // Number of property set
+	Input >> nset;	// Number of property set
 
-    if (nset != mset + 1)
-    {
-        cerr << "*** Error *** Material sets must be inputted in order !" << endl
-             << "    Expected set : " << mset + 1 << endl
-             << "    Provided set : " << nset << endl;
+	if (nset != mset + 1)
+	{
+		cerr << "*** Error *** Material sets must be inputted in order !" << endl 
+			 << "    Expected set : " << mset + 1 << endl
+			 << "    Provided set : " << nset << endl;
 
-        return false;
-    }
+		return false;
+	}
 
-    Input >> E >> nu; // Young's modulus and Poisson's ratio
+	Input >> E >> nu;	// Young's modulus and Poisson's ratio
 
-    return true;
+	return true;
 }
 
 
@@ -113,7 +113,7 @@ void CTriangleMaterial::Write(COutputter& output, unsigned int mset)
 //	Write material data to Stream
 void CQuadrilateralMaterial::Write(COutputter& output, unsigned int mset)
 {
-    output << setw(5) << mset + 1 << setw(16) << E << setw(16) << nu << endl;
+	output << setw(5) << mset+1 << setw(16) << E << setw(16) << nu << endl;
 }
 
 //	Read material data from stream Input
@@ -151,12 +151,14 @@ bool CTimoshenkoMaterial::Read(ifstream& Input, unsigned int mset)
 		cerr << "*** Error *** Material sets must be inputted in order !" << endl
 			<< "    Expected set : " << mset + 1 << endl
 			<< "    Provided set : " << nset << endl;
+
 		return false;
 	}
 
 	Input >> E >> nu >> Area;	// Young's modulus, Poisson's ratio and Section area
 	Input >> Iyy >> Izz; 		// Moment of inertia for bending about local axis
 	Input >> Thetay1 >> Thetay2 >> Thetay3;
+	// Direction cosine of local axis
 
 	return true;
 }
@@ -199,7 +201,6 @@ void CPlateMaterial::Write(COutputter& output, unsigned int mset)
     output << setw(5) << mset + 1 << setw(16) << E << setw(16) << h << setw(16) << nu << endl;
 }
 
-
 bool CShellMaterial::Read(ifstream& Input, unsigned int mset)
 {
     Input >> nset; // Number of property set
@@ -222,4 +223,3 @@ void CShellMaterial::Write(COutputter& output, unsigned int mset)
 {
     output << setw(5) << mset + 1 << setw(16) << E << setw(16) << h << setw(16) << nu << endl;
 }
-
