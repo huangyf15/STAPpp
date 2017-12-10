2caae04666fcd287801d039ffb650089ecbe53e7
gwy15 <gwy15thu@gmail.com>
2017-12-11 00:04:46 +0800

    resolve Element.cpp

--- src/cpp/Element.cpp
+++ src/cpp/Element.cpp
@@ -8,78 +8,74 @@
 /*     http://www.comdyn.cn/                                                 */
 /*****************************************************************************/
 
-#include "Element.h"
 #include <algorithm>
 #include <climits>
+#include "Element.h"
 
 //! Virtual deconstructor
 CElement::~CElement()
 {
     if (!nodes)
-        delete[] nodes;
-
+        delete [] nodes;
+    
     // if (!ElementMaterial)
     //     delete [] ElementMaterial;
 
     if (!LocationMatrix)
-        delete[] LocationMatrix;
+        delete [] LocationMatrix;
 }
 
 //  Calculate the column height, used with the skyline storage scheme
 void CElement::CalculateColumnHeight(unsigned int* ColumnHeight)
 {
-
-    //  Generate location matrix
+    
+//  Generate location matrix
     GenerateLocationMatrix();
 
-    //  Look for the row number of the first non-zero element
+//  Look for the row number of the first non-zero element
     unsigned int nfirstrow = INT_MAX;
     for (unsigned int i = 0; i < ND; i++)
         if (LocationMatrix[i] && LocationMatrix[i] < nfirstrow)
             nfirstrow = LocationMatrix[i];
 
-    //	Calculate the column height contributed by this element
-    for (unsigned int i = 0; i < ND; i++)
-    {
-        unsigned int column = LocationMatrix[i];
-        if (!column)
-            continue;
+//	Calculate the column height contributed by this element
+	for (unsigned int i = 0; i < ND; i++)
+	{
+		unsigned int column = LocationMatrix[i];
+		if (!column)
+			continue;
 
-        const unsigned int Height = column - nfirstrow;
-        if (ColumnHeight[column - 1] < Height)
-            ColumnHeight[column - 1] = Height;
-    }
+		const unsigned int Height = column - nfirstrow;
+		if (ColumnHeight[column-1] < Height) ColumnHeight[column-1] = Height;
+	}
 }
 
 //	Assemble the banded global stiffness matrix (skyline storage scheme)
 void CElement::assembly(double* Matrix, CSkylineMatrix<double>* StiffnessMatrix)
 {
-    //	Calculate element stiffness matrix
-    ElementStiffness(Matrix);
+//	Calculate element stiffness matrix
+	ElementStiffness(Matrix);
+	
+//	Assemble global stiffness matrix
+	for (unsigned int j = 0; j < ND; j++)
+	{
+		unsigned int Lj = LocationMatrix[j];	// Global equation number corresponding to jth DOF of the element
+		if (!Lj) 
+			continue;
 
-    //	Assemble global stiffness matrix
-    for (unsigned int j = 0; j < ND; j++)
-    {
-        unsigned int Lj =
-            LocationMatrix[j]; // Global equation number corresponding to jth DOF of the element
-        if (!Lj)
-            continue;
+//		Address of diagonal element of column j in the one dimensional element stiffness matrix
+		unsigned int DiagjElement = (j+1)*j/2 + 1;
 
-        //		Address of diagonal element of column j in the one dimensional element stiffness
-        //matrix
-        unsigned int DiagjElement = (j + 1) * j / 2 + 1;
+		for (unsigned int i = 0; i <= j; i++)
+		{
+			unsigned int Li = LocationMatrix[i];	// Global equation number corresponding to ith DOF of the element
 
-        for (unsigned int i = 0; i <= j; i++)
-        {
-            unsigned int Li =
-                LocationMatrix[i]; // Global equation number corresponding to ith DOF of the element
+			if (!Li)
+				continue;
+            
+            (*StiffnessMatrix)(Li,Lj) += Matrix[DiagjElement + j - i - 1];
+		}
+	}
 
-            if (!Li)
-                continue;
-
-            (*StiffnessMatrix)(Li, Lj) += Matrix[DiagjElement + j - i - 1];
-        }
-    }
-
-    return;
+	return;
 }
