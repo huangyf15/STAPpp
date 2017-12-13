0559e4cdc4e1ccb14ff2299186397e8c9792071f
dbyboy <805364314@qq.com>
2017-12-13 15:37:03 +0800

    删除中文注释

--- src/cpp/Elements/Beam.cpp
+++ src/cpp/Elements/Beam.cpp
@@ -121,9 +121,9 @@ void CBeam::ElementStiffness(double* Matrix)
     for (unsigned int i = 0; i < 3; i++)
         n[0][i] = DX[i] / L; //orientation of x'
 
-    n[1][0] = material->n1;
-    n[1][1] = material->n2;
-    n[1][2] = material->n3; // orientation of y'
+    n[1][0] = material.n1;
+    n[1][1] = material.n2;
+    n[1][2] = material.n3; // orientation of y'
     n[2][0] = n[0][1] * n[1][2] - n[0][2] * n[1][1];
     n[2][1] = n[0][2] * n[1][0] - n[0][0] * n[1][2];
     n[2][2] = n[0][0] * n[1][1] - n[0][1] * n[1][0]; //orientation of z'
