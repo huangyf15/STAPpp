cf51132c237737137362027629a537356ef3f372
dbyboy <805364314@qq.com>
2017-12-24 21:39:19 +0800

    完成算例

--- src/cpp/Elements/Infinite_4Q.cpp
+++ src/cpp/Elements/Infinite_4Q.cpp
@@ -10,47 +10,45 @@
 
 #include "Elements/Infinite_4Q.h"
 
-#include <iostream>
-#include <iomanip>
 #include <cmath>
+#include <iomanip>
+#include <iostream>
 
 using namespace std;
 
 //	Constructor
 CInfEle::CInfEle()
 {
-    NEN = 4;	// Each element has 4 nodes
+    NEN = 4; // Each element has 4 nodes
     nodes = new CNode*[NEN];
-    
-    ND = 12;
+
+    ND = 8;
     LocationMatrix = new unsigned int[ND];
 
     ElementMaterial = nullptr;
 }
 
 //	Desconstructor
-CInfEle::~CInfEle()
-{
-}
+CInfEle::~CInfEle() {}
 
 //	Read element data from stream Input
 bool CInfEle::Read(ifstream& Input, unsigned int Ele, CMaterial* MaterialSets, CNode* NodeList)
 {
     unsigned int N;
 
-    Input >> N;	// element number
+    Input >> N; // element number
 
     if (N != Ele + 1)
     {
-        cerr << "*** Error *** Elements must be inputted in order !" << endl 
+        cerr << "*** Error *** Elements must be inputted in order !" << endl
              << "   Expected element : " << Ele + 1 << endl
              << "   Provided element : " << N << endl;
 
         return false;
     }
 
-    unsigned int MSet;	// Material property set number
-    unsigned int N1, N2, N3, N4;	// numbers of 4 Nodes
+    unsigned int MSet;           // Material property set number
+    unsigned int N1, N2, N3, N4; // numbers of 4 Nodes
 
     Input >> N1 >> N2 >> N3 >> N4 >> MSet;
     ElementMaterial = static_cast<CInfiniteMaterial*>(MaterialSets) + MSet - 1;
@@ -65,39 +63,40 @@ bool CInfEle::Read(ifstream& Input, unsigned int Ele, CMaterial* MaterialSets, C
 //	Write element data to stream OutputFile
 void CInfEle::Write(COutputter& output, unsigned int Ele)
 {
-	output << setw(5) << Ele+1 << setw(11) << nodes[0]->NodeNumber 
-		   << setw(9) << nodes[1]->NodeNumber << setw(9) << nodes[2]->NodeNumber
-           << setw(9) << nodes[3]->NodeNumber << setw(12) << ElementMaterial->nset << endl;
+    output << setw(5) << Ele + 1 << setw(11) << nodes[0]->NodeNumber << setw(9)
+           << nodes[1]->NodeNumber << setw(9) << nodes[2]->NodeNumber << setw(9)
+           << nodes[3]->NodeNumber << setw(12) << ElementMaterial->nset << endl;
 }
 
-//  Generate location matrix: the global equation number that corresponding to each DOF of the element
+//  Generate location matrix: the global equation number that corresponding to each DOF of the
+//  element
 //	Caution:  Equation number is numbered from 1 !
 void CInfEle::GenerateLocationMatrix()
 {
     unsigned int i = 0;
     for (unsigned int N = 0; N < NEN; N++)
-        for (unsigned int D = 0; D < 3; D++)
+        for (unsigned int D = 0; D < 2; D++)
             LocationMatrix[i++] = nodes[N]->bcode[D];
 }
-
 
 //	Return the size of the element stiffness matrix (stored as an array column by column)
 //	For infinite 4Q element, element stiffness is a 8x8 matrix, whose upper triangular part
 //	has 36 elements
 unsigned int CInfEle::SizeOfStiffnessMatrix() { return 36; }
 
-//	Calculate element stiffness matrix 
+//	Calculate element stiffness matrix
 //	Upper triangular matrix, stored as an array column by colum starting from the diagonal element
 void CInfEle::ElementStiffness(double* Matrix)
 {
     clear(Matrix, SizeOfStiffnessMatrix());
 
-//	Calculate element stiffness matrix
+    //	Calculate element stiffness matrix
 
-    const CInfiniteMaterial& material = static_cast<CInfiniteMaterial&>(*ElementMaterial);	// Pointer to material of the element
+    const CInfiniteMaterial& material =
+        static_cast<CInfiniteMaterial&>(*ElementMaterial); // Pointer to material of the element
 
     double E = material.E;
-    double nu = material.nu;  
+    double nu = material.nu;
     double eta, psi, Je;
     double J[2][2];
     double JT[2][2];
@@ -105,134 +104,106 @@ void CInfEle::ElementStiffness(double* Matrix)
     double Ke[36];
     double D[4];
     double x[4], y[4];
-    for (unsigned i=0 ; i<36 ;i++){
-        Ke[i]=0;
+    for (unsigned i = 0; i < 36; i++)
+    {
+        Ke[i] = 0;
     }
 
     x[0] = nodes[0]->XYZ[0];
     y[0] = nodes[0]->XYZ[1];
     x[1] = nodes[1]->XYZ[0];
     y[1] = nodes[1]->XYZ[1];
-    x[2] = 2 * x[0] - nodes[2]->XYZ[0];
-    y[2] = 2 * y[0] - nodes[2]->XYZ[1];
-    x[3] = 2 * x[1] - nodes[3]->XYZ[0];
-    y[3] = 2 * y[1] - nodes[3]->XYZ[1];
+    x[2] = nodes[2]->XYZ[0];
+    y[2] = nodes[2]->XYZ[1];
+    x[3] = nodes[3]->XYZ[0];
+    y[3] = nodes[3]->XYZ[1];
 
-    for (unsigned i=0 ; i<2 ;i++){
-        for (unsigned j=0; j<2 ; j++){
+    for (unsigned i = 0; i < 2; i++)
+    {
+        for (unsigned j = 0; j < 2; j++)
+        {
 
-            eta=pow(-1,i+1)/sqrt(3);
-            psi=pow(-1,j+1)/sqrt(3); //Gauss point in local coordinate 
+            eta = pow(-1, i + 1) / sqrt(3);
+            psi = pow(-1, j + 1) / sqrt(3); // Gauss point in local coordinate
 
-            J[0][0]=x[0]*(eta-1)/((1-psi)*(1-psi))+x[1]*(-1-eta)/((1-psi)*(1-psi))+x[2]*(1+eta)/((1-psi)*(1-psi))+x[3]*(1-eta)/((1-psi)*(1-psi));
-            J[0][1]=y[0]*(eta-1)/((1-psi)*(1-psi))+y[1]*(-1-eta)/((1-psi)*(1-psi))+y[2]*(1+eta)/((1-psi)*(1-psi))+y[3]*(1-eta)/((1-psi)*(1-psi));
-            J[1][0]=x[0]*psi/(1-psi)+x[1]*(-psi)/(1-psi)+x[2]*(1+psi)/(2*(1-psi))+x[3]*(-1-psi)/(2*(1-psi));
-            J[1][1]=y[0]*psi/(1-psi)+y[1]*(-psi)/(1-psi)+y[2]*(1+psi)/(2*(1-psi))+y[3]*(-1-psi)/(2*(1-psi));
-            Je=fabs(J[0][0]*J[1][1]-J[0][1]*J[1][0]);
-            JT[0][0]=J[1][1]/Je;
-            JT[0][1]=-J[0][1]/Je;
-            JT[1][0]=-J[1][0]/Je;
-            JT[1][1]=J[0][0]/Je;
-            
-            B[0][0]=JT[0][0]*(eta-1)/((1-psi)*(1-psi))+JT[0][1]*psi/(1-psi);
-            B[0][1]=JT[0][0]*(-1-eta)/((1-psi)*(1-psi))+JT[0][1]*(-psi)/(1-psi);
-            B[0][2]=JT[0][0]*(1+eta)/((1-psi)*(1-psi))+JT[0][1]*(psi+1)/(2*(1-psi));
-            B[0][3]=JT[0][0]*(1-eta)/((1-psi)*(1-psi))+JT[0][1]*(-1-psi)/(2*(1-psi));
-            B[1][0]=JT[1][0]*(eta-1)/((1-psi)*(1-psi))+JT[1][1]*psi/(1-psi);
-            B[1][1]=JT[1][0]*(-1-eta)/((1-psi)*(1-psi))+JT[1][1]*(-psi)/(1-psi);
-            B[1][2]=JT[1][0]*(1+eta)/((1-psi)*(1-psi))+JT[1][1]*(psi+1)/(2*(1-psi));
-            B[1][3]=JT[1][0]*(1-eta)/((1-psi)*(1-psi))+JT[1][1]*(-1-psi)/(2*(1-psi));
+            J[0][0] = x[0] * (eta - 1) / ((1 - psi) * (1 - psi)) +
+                      x[1] * (-1 - eta) / ((1 - psi) * (1 - psi)) +
+                      x[2] * (1 + eta) / ((1 - psi) * (1 - psi)) +
+                      x[3] * (1 - eta) / ((1 - psi) * (1 - psi));
+            J[0][1] = y[0] * (eta - 1) / ((1 - psi) * (1 - psi)) +
+                      y[1] * (-1 - eta) / ((1 - psi) * (1 - psi)) +
+                      y[2] * (1 + eta) / ((1 - psi) * (1 - psi)) +
+                      y[3] * (1 - eta) / ((1 - psi) * (1 - psi));
+            J[1][0] = x[0] * psi / (1 - psi) + x[1] * (-psi) / (1 - psi) +
+                      x[2] * (1 + psi) / (2 * (1 - psi)) + x[3] * (-1 - psi) / (2 * (1 - psi));
+            J[1][1] = y[0] * psi / (1 - psi) + y[1] * (-psi) / (1 - psi) +
+                      y[2] * (1 + psi) / (2 * (1 - psi)) + y[3] * (-1 - psi) / (2 * (1 - psi));
+            Je = fabs(J[0][0] * J[1][1] - J[0][1] * J[1][0]);
+            JT[0][0] = J[1][1] / Je;
+            JT[0][1] = -J[0][1] / Je;
+            JT[1][0] = -J[1][0] / Je;
+            JT[1][1] = J[0][0] / Je;
 
-            D[0]=E/(1-nu*nu);
-            D[1]=nu*E/(1-nu*nu);
-            D[2]=E/(1-nu*nu);
-            D[3]=(1-nu)*E/(2-2*nu*nu);
+            B[0][0] = JT[0][0] * (eta - 1) / ((1 - psi) * (1 - psi)) + JT[0][1] * psi / (1 - psi);
+            B[0][1] =
+                JT[0][0] * (-1 - eta) / ((1 - psi) * (1 - psi)) + JT[0][1] * (-psi) / (1 - psi);
+            B[0][2] = JT[0][0] * (1 + eta) / ((1 - psi) * (1 - psi)) +
+                      JT[0][1] * (psi + 1) / (2 * (1 - psi));
+            B[0][3] = JT[0][0] * (1 - eta) / ((1 - psi) * (1 - psi)) +
+                      JT[0][1] * (-1 - psi) / (2 * (1 - psi));
+            B[1][0] = JT[1][0] * (eta - 1) / ((1 - psi) * (1 - psi)) + JT[1][1] * psi / (1 - psi);
+            B[1][1] =
+                JT[1][0] * (-1 - eta) / ((1 - psi) * (1 - psi)) + JT[1][1] * (-psi) / (1 - psi);
+            B[1][2] = JT[1][0] * (1 + eta) / ((1 - psi) * (1 - psi)) +
+                      JT[1][1] * (psi + 1) / (2 * (1 - psi));
+            B[1][3] = JT[1][0] * (1 - eta) / ((1 - psi) * (1 - psi)) +
+                      JT[1][1] * (-1 - psi) / (2 * (1 - psi));
 
+            D[0] = E / (1 - nu * nu);
+            D[1] = nu * E / (1 - nu * nu);
+            D[2] = E / (1 - nu * nu);
+            D[3] = (1 - nu) * E / (2 - 2 * nu * nu);
 
-            Ke[0]+=Je*(B[0][0]*B[0][0]*D[0]+B[1][0]*B[1][0]*D[3]);
-            Ke[1]+=Je*(B[0][0]*B[1][0]*(D[1]+D[3]));
-            Ke[2]+=Je*(B[1][0]*B[1][0]*D[2]+B[0][0]*B[0][0]*D[3]);
-            Ke[3]+=Je*(B[0][0]*B[0][1]*D[0]+B[1][0]*B[1][1]*D[3]);
-            Ke[4]+=Je*(B[1][0]*B[0][1]*D[1]+B[0][0]*B[1][1]*D[3]);
-            Ke[5]+=Je*(B[0][1]*B[0][1]*D[0]+B[1][1]*B[1][1]*D[3]);
-            Ke[6]+=Je*(B[0][0]*B[1][1]*D[1]+B[1][0]*B[0][1]*D[3]);
-            Ke[7]+=Je*(B[1][0]*B[1][1]*D[2]+B[0][0]*B[0][1]*D[3]);
-            Ke[8]+=Je*(B[0][1]*B[1][1]*D[1]+B[1][1]*B[0][1]*D[3]);
-            Ke[9]+=Je*(B[1][1]*B[1][1]*D[2]+B[0][1]*B[0][1]*D[3]);
-            Ke[10]+=Je*(B[0][0]*B[0][2]*D[0]+B[1][0]*B[1][2]*D[3]);
-            Ke[11]+=Je*(B[1][0]*B[0][2]*D[1]+B[0][0]*B[1][2]*D[3]);
-            Ke[12]+=Je*(B[0][1]*B[0][2]*D[0]+B[1][1]*B[1][2]*D[3]);
-            Ke[13]+=Je*(B[1][1]*B[0][2]*D[1]+B[0][1]*B[1][2]*D[3]);
-            Ke[14]+=Je*(B[0][2]*B[0][2]*D[0]+B[1][2]*B[1][2]*D[3]);
-            Ke[15]+=Je*(B[0][0]*B[1][2]*D[1]+B[1][0]*B[0][2]*D[3]);
-            Ke[16]+=Je*(B[1][0]*B[1][2]*D[2]+B[0][0]*B[0][2]*D[3]);
-            Ke[17]+=Je*(B[0][1]*B[1][2]*D[1]+B[1][1]*B[0][2]*D[3]);
-            Ke[18]+=Je*(B[1][1]*B[1][2]*D[2]+B[0][1]*B[0][2]*D[3]);
-            Ke[19]+=Je*(B[0][2]*B[1][2]*D[1]+B[1][2]*B[0][2]*D[3]);
-            Ke[20]+=Je*(B[1][2]*B[1][2]*D[2]+B[0][2]*B[0][2]*D[3]);
-            Ke[21]+=Je*(B[0][0]*B[0][3]*D[0]+B[1][0]*B[1][3]*D[3]);
-            Ke[22]+=Je*(B[1][0]*B[0][3]*D[1]+B[0][0]*B[1][3]*D[3]);
-            Ke[23]+=Je*(B[0][1]*B[0][3]*D[0]+B[1][1]*B[1][3]*D[3]);
-            Ke[24]+=Je*(B[1][1]*B[0][3]*D[1]+B[0][1]*B[1][3]*D[3]);
-            Ke[25]+=Je*(B[0][2]*B[0][3]*D[0]+B[1][2]*B[1][3]*D[3]);
-            Ke[26]+=Je*(B[1][2]*B[0][3]*D[1]+B[0][2]*B[1][3]*D[3]);
-            Ke[27]+=Je*(B[0][3]*B[0][3]*D[0]+B[1][3]*B[1][3]*D[3]);
-            Ke[28]+=Je*(B[0][0]*B[1][3]*D[1]+B[1][0]*B[0][3]*D[3]);
-            Ke[29]+=Je*(B[1][0]*B[1][3]*D[2]+B[0][0]*B[0][3]*D[3]);
-            Ke[30]+=Je*(B[0][1]*B[1][3]*D[1]+B[1][1]*B[0][3]*D[3]);
-            Ke[31]+=Je*(B[1][1]*B[1][3]*D[2]+B[0][1]*B[0][3]*D[3]);
-            Ke[32]+=Je*(B[0][2]*B[1][3]*D[1]+B[1][2]*B[0][3]*D[3]);
-            Ke[33]+=Je*(B[1][2]*B[1][3]*D[2]+B[0][2]*B[0][3]*D[3]);
-            Ke[34]+=Je*(B[0][3]*B[1][3]*D[1]+B[1][3]*B[0][3]*D[3]);
-            Ke[35]+=Je*(B[1][3]*B[1][3]*D[2]+B[0][3]*B[0][3]*D[3]);
-
-
-            Matrix[0]+=Je*(B[0][0]*B[0][0]*D[0]+B[1][0]*B[1][0]*D[3]);
-            Matrix[1]+=Je*(B[0][0]*B[1][0]*(D[1]+D[3]));
-            Matrix[2]+=Je*(B[1][0]*B[1][0]*D[2]+B[0][0]*B[0][0]*D[3]);
-            Matrix[3]+=Je*(B[0][0]*B[0][1]*D[0]+B[1][0]*B[1][1]*D[3]);
-            Matrix[4]+=Je*(B[1][0]*B[0][1]*D[1]+B[0][0]*B[1][1]*D[3]);
-            Matrix[5]+=Je*(B[0][1]*B[0][1]*D[0]+B[1][1]*B[1][1]*D[3]);
-            Matrix[6]+=Je*(B[0][0]*B[1][1]*D[1]+B[1][0]*B[0][1]*D[3]);
-            Matrix[7]+=Je*(B[1][0]*B[1][1]*D[2]+B[0][0]*B[0][1]*D[3]);
-            Matrix[8]+=Je*(B[0][1]*B[1][1]*D[1]+B[1][1]*B[0][1]*D[3]);
-            Matrix[9]+=Je*(B[1][1]*B[1][1]*D[2]+B[0][1]*B[0][1]*D[3]);
-            Matrix[10]+=Je*(B[0][0]*B[0][2]*D[0]+B[1][0]*B[1][2]*D[3]);
-            Matrix[11]+=Je*(B[1][0]*B[0][2]*D[1]+B[0][0]*B[1][2]*D[3]);
-            Matrix[12]+=Je*(B[0][1]*B[0][2]*D[0]+B[1][1]*B[1][2]*D[3]);
-            Matrix[13]+=Je*(B[1][1]*B[0][2]*D[1]+B[0][1]*B[1][2]*D[3]);
-            Matrix[14]+=Je*(B[0][2]*B[0][2]*D[0]+B[1][2]*B[1][2]*D[3]);
-            Matrix[15]+=Je*(B[0][0]*B[1][2]*D[1]+B[1][0]*B[0][2]*D[3]);
-            Matrix[16]+=Je*(B[1][0]*B[1][2]*D[2]+B[0][0]*B[0][2]*D[3]);
-            Matrix[17]+=Je*(B[0][1]*B[1][2]*D[1]+B[1][1]*B[0][2]*D[3]);
-            Matrix[18]+=Je*(B[1][1]*B[1][2]*D[2]+B[0][1]*B[0][2]*D[3]);
-            Matrix[19]+=Je*(B[0][2]*B[1][2]*D[1]+B[1][2]*B[0][2]*D[3]);
-            Matrix[20]+=Je*(B[1][2]*B[1][2]*D[2]+B[0][2]*B[0][2]*D[3]);
-            Matrix[21]+=Je*(B[0][0]*B[0][3]*D[0]+B[1][0]*B[1][3]*D[3]);
-            Matrix[22]+=Je*(B[1][0]*B[0][3]*D[1]+B[0][0]*B[1][3]*D[3]);
-            Matrix[23]+=Je*(B[0][1]*B[0][3]*D[0]+B[1][1]*B[1][3]*D[3]);
-            Matrix[24]+=Je*(B[1][1]*B[0][3]*D[1]+B[0][1]*B[1][3]*D[3]);
-            Matrix[25]+=Je*(B[0][2]*B[0][3]*D[0]+B[1][2]*B[1][3]*D[3]);
-            Matrix[26]+=Je*(B[1][2]*B[0][3]*D[1]+B[0][2]*B[1][3]*D[3]);
-            Matrix[27]+=Je*(B[0][3]*B[0][3]*D[0]+B[1][3]*B[1][3]*D[3]);
-            Matrix[28]+=Je*(B[0][0]*B[1][3]*D[1]+B[1][0]*B[0][3]*D[3]);
-            Matrix[29]+=Je*(B[1][0]*B[1][3]*D[2]+B[0][0]*B[0][3]*D[3]);
-            Matrix[30]+=Je*(B[0][1]*B[1][3]*D[1]+B[1][1]*B[0][3]*D[3]);
-            Matrix[31]+=Je*(B[1][1]*B[1][3]*D[2]+B[0][1]*B[0][3]*D[3]);
-            Matrix[32]+=Je*(B[0][2]*B[1][3]*D[1]+B[1][2]*B[0][3]*D[3]);
-            Matrix[33]+=Je*(B[1][2]*B[1][3]*D[2]+B[0][2]*B[0][3]*D[3]);
-            Matrix[34]+=Je*(B[0][3]*B[1][3]*D[1]+B[1][3]*B[0][3]*D[3]);
-            Matrix[35]+=Je*(B[1][3]*B[1][3]*D[2]+B[0][3]*B[0][3]*D[3]);
+            Matrix[0] += Je * (B[0][0] * B[0][0] * D[0] + B[1][0] * B[1][0] * D[3]);
+            Matrix[1] += Je * (B[1][0] * B[1][0] * D[2] + B[0][0] * B[0][0] * D[3]);
+            Matrix[2] += Je * (B[0][0] * B[1][0] * (D[1] + D[3]));
+            Matrix[3] += Je * (B[0][1] * B[0][1] * D[0] + B[1][1] * B[1][1] * D[3]);
+            Matrix[4] += Je * (B[1][0] * B[0][1] * D[1] + B[0][0] * B[1][1] * D[3]);
+            Matrix[5] += Je * (B[0][0] * B[0][1] * D[0] + B[1][0] * B[1][1] * D[3]);
+            Matrix[6] += Je * (B[1][1] * B[1][1] * D[2] + B[0][1] * B[0][1] * D[3]);
+            Matrix[7] += Je * (B[0][1] * B[1][1] * D[1] + B[1][1] * B[0][1] * D[3]);
+            Matrix[8] += Je * (B[1][0] * B[1][1] * D[2] + B[0][0] * B[0][1] * D[3]);
+            Matrix[9] += Je * (B[0][0] * B[1][1] * D[1] + B[1][0] * B[0][1] * D[3]);
+            Matrix[10] += Je * (B[0][2] * B[0][2] * D[0] + B[1][2] * B[1][2] * D[3]);
+            Matrix[11] += Je * (B[1][1] * B[0][2] * D[1] + B[0][1] * B[1][2] * D[3]);
+            Matrix[12] += Je * (B[0][1] * B[0][2] * D[0] + B[1][1] * B[1][2] * D[3]);
+            Matrix[13] += Je * (B[1][0] * B[0][2] * D[1] + B[0][0] * B[1][2] * D[3]);
+            Matrix[14] += Je * (B[0][0] * B[0][2] * D[0] + B[1][0] * B[1][2] * D[3]);
+            Matrix[15] += Je * (B[1][2] * B[1][2] * D[2] + B[0][2] * B[0][2] * D[3]);
+            Matrix[16] += Je * (B[0][2] * B[1][2] * D[1] + B[1][2] * B[0][2] * D[3]);
+            Matrix[17] += Je * (B[1][1] * B[1][2] * D[2] + B[0][1] * B[0][2] * D[3]);
+            Matrix[18] += Je * (B[0][1] * B[1][2] * D[1] + B[1][1] * B[0][2] * D[3]);
+            Matrix[19] += Je * (B[1][0] * B[1][2] * D[2] + B[0][0] * B[0][2] * D[3]);
+            Matrix[20] += Je * (B[0][0] * B[1][2] * D[1] + B[1][0] * B[0][2] * D[3]);
+            Matrix[21] += Je * (B[0][3] * B[0][3] * D[0] + B[1][3] * B[1][3] * D[3]);
+            Matrix[22] += Je * (B[1][2] * B[0][3] * D[1] + B[0][2] * B[1][3] * D[3]);
+            Matrix[23] += Je * (B[0][2] * B[0][3] * D[0] + B[1][2] * B[1][3] * D[3]);
+            Matrix[24] += Je * (B[1][1] * B[0][3] * D[1] + B[0][1] * B[1][3] * D[3]);
+            Matrix[25] += Je * (B[0][1] * B[0][3] * D[0] + B[1][1] * B[1][3] * D[3]);
+            Matrix[26] += Je * (B[1][0] * B[0][3] * D[1] + B[0][0] * B[1][3] * D[3]);
+            Matrix[27] += Je * (B[0][0] * B[0][3] * D[0] + B[1][0] * B[1][3] * D[3]);
+            Matrix[28] += Je * (B[1][3] * B[1][3] * D[2] + B[0][3] * B[0][3] * D[3]);
+            Matrix[29] += Je * (B[0][3] * B[1][3] * D[1] + B[1][3] * B[0][3] * D[3]);
+            Matrix[30] += Je * (B[1][2] * B[1][3] * D[2] + B[0][2] * B[0][3] * D[3]);
+            Matrix[31] += Je * (B[0][2] * B[1][3] * D[1] + B[1][2] * B[0][3] * D[3]);
+            Matrix[32] += Je * (B[1][1] * B[1][3] * D[2] + B[0][1] * B[0][3] * D[3]);
+            Matrix[33] += Je * (B[0][1] * B[1][3] * D[1] + B[1][1] * B[0][3] * D[3]);
+            Matrix[34] += Je * (B[1][0] * B[1][3] * D[2] + B[0][0] * B[0][3] * D[3]);
+            Matrix[35] += Je * (B[0][0] * B[1][3] * D[1] + B[1][0] * B[0][3] * D[3]);
         }
     }
 }
 
-//	Calculate element stress 
-void CInfEle::ElementStress(double* Q4stress, double* Displacement)
-{
-
-}
-void CInfEle::ElementGauss(double* Coordinate){
-
-}
-
+//	Calculate element stress
+void CInfEle::ElementStress(double* Q4stress, double* Displacement) {}
+void CInfEle::ElementGauss(double* Coordinate) {}
