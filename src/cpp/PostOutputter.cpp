8f61f155db4e666866a1419a09be09ab6511ec83
huangyf15 <1466879176@qq.com>
2017-12-24 17:14:44 +0800

    modify the amplification factor to match ABAQUS

--- src/cpp/PostOutputter.cpp
+++ src/cpp/PostOutputter.cpp
@@ -31,7 +31,7 @@ void PostOutputter::OutputElementStress()
     // The amplification factor coeff
     // Output = InitPosition + coeff * Displacement
     //        = InitPosition + coeff * (FinalPosition - InitPosition);
-    double coeff = 50.0;
+    double coeff = 107.7;
 
     CDomain* FEMData = CDomain::Instance();
 
