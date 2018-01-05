863ebaffa8c228b6da84e95ae28671d7d9f9b92c
huangyf15 <1466879176@qq.com>
2018-01-05 23:16:36 +0800

    fix bugs in frustum.cpp, update the patchtest file

--- memo/frustum/frustum.m
+++ memo/frustum/frustum.m
@@ -1,29 +1,68 @@
-function frustum
-syms B BT eta C S l r z;
+function [Matrix0] = frustum
+syms B BT eta;
+syms C S l r0 k nu h;
+syms xi1 xi2 weight1 weight2;
+syms prmeter;
 
-B(1,1:6) = [-1/l, -z/l/l*(12*eta - 6), -z/l*(6*eta - 4), 1/l, -z/l*(-12*eta + 6), -z/l*(6*eta - 2)];
-B(2,1) = (1-eta)*S/r;
-B(2,2) = 1/r*(C*(2*eta*eta*eta - 3*eta*eta + 1) - S/l*(6*eta*eta - 6*eta)); 
-B(2,3) = l/r*(C*(eta*eta*eta - 2*eta*eta + eta) - S/l*(3*eta*eta - 4*eta + 1));
-B(2,4) = eta/r*S;
-B(2,5) = 1/r*(C*(- 2*eta*eta*eta + 3*eta*eta) - S/l*( - 6*eta*eta + 6*eta));
-B(2,6) = l/r*(C*(eta*eta*eta - eta*eta) - S/l*(3*eta*eta - 2*eta));
+% prmeter = 2*pi*l
+% k = E * h / (1 - nu*nu)
 
-for i = 1:2
+% C = sqrt(2)/2;
+% S = sqrt(2)/2;
+% r0 = 0.1;
+% l = 0.2;
+% xi1 = 0.8611363116;
+% xi2 = 0.3399810436;
+% weight1 = 0.3478548451;
+% weight2 = 0.6521451549;
+% k = 1000;
+% nu = 0.2;
+% h = 0.2;
+% prmeter = 2*pi*l;
+
+r = r0 + l * S * eta;
+B(1,1) = -1/l;
+B(1,4) = 1/l;
+B(2,1) = (1 - eta) * S/r;
+B(2,2) = (1 - 3*eta*eta + 2*eta*eta*eta) * C/r;
+B(2,3) = l * (eta - 2*eta*eta + eta*eta*eta) * C/r;
+B(2,4) = eta * S/r;
+B(2,5) = (3*eta*eta - 2*eta*eta*eta) * C/r;
+B(2,6) = l * (-eta*eta + eta*eta*eta) * C/r;
+B(3,2) = -(-6 + 12*eta) / l / l;
+B(3,3) = -(-4 + 6*eta) / l;
+B(3,5) = -(6 - 12*eta) / l / l;
+B(3,6) = -(-2 + 6*eta) / l;
+B(4,2) = -(-6*eta + 6*eta*eta) * S/r/l;
+B(4,3) = -(1 - 4*eta + 3*eta*eta) * S/r;
+B(4,5) = -(6*eta - 6*eta*eta) * S/r/l;
+B(4,6) = -(-2*eta + 3*eta*eta) * S/r;
+
+for i = 1:4
     for j = 1:6
         BT(j,i) = B(i,j);
     end
 end
 
-syms k nu D;
-D = k.*[1 nu
-        nu 1];
-syms Ke;
+% Calculate element stiffness matrix
+D = sym(zeros(4));
+D(1:2,1:2) = k.*[1 nu 
+                 nu 1];
+D(3:4,3:4) = h*h/12 * D(1:2,1:2);
 Ke = BT * D * B;
 
-syms h T TT;
-f2 = int(2*pi*l*int(r.*Ke,eta,0,1),z,-h/2,h/2);
+% Numerical integration
+f0 = sym(zeros(6));
+for i = 1:6
+    for j = 1:6
+        f0(i,j) = weight1 * subs(r*Ke(i,j),eta,(1+xi1)*0.5) ...
+                  + weight1 * subs(r*Ke(i,j),eta,(1-xi1)*0.5) ...
+                  + weight2 * subs(r*Ke(i,j),eta,(1+xi2)*0.5) ...
+                  + weight2 * subs(r*Ke(i,j),eta,(1-xi2)*0.5);
+    end
+end
 
+% Transform to the background coordinate
 T = [C S 0 0 0 0
      -S C 0 0 0 0 
      0 0 1 0 0 0 
@@ -35,9 +74,9 @@ for i = 1:6
         TT(j,i) = T(i,j);
     end
 end
+ff = prmeter*TT*f0*T;
 
-ff = TT * f2 * T;
-
+% Transform to the symbolic form
 syms Matrix Matrixp0p;
 Matrix = sym(zeros(1,21));
 s = 0;
@@ -48,5 +87,5 @@ for j = 1:6
     end
 end
 
-ccode(Matrix(1,:))
+Matrix0 = ccode(Matrix(1,:));
 end
